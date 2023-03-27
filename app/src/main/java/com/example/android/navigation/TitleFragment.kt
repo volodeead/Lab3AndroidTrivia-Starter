20a3ab4c2d8522ee8a1d16a3bc4d68f0de96e04a
Dyhodyk Volodia <volodeead.v@gmail.com>
2023-03-27 18:59:22 +0300

    Rewrite the TitleFragment according to the last task, End of step 1.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,5 +1,6 @@
 package com.example.android.navigation
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,10 +9,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 class TitleFragment : Fragment() {
 
